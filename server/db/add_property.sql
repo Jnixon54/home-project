54ff0a5ff1ed10217641175dd7c37edd63d189da
mcarson01 <matthcarson01@gmail.com>
2017-11-03 12:00:37 -0500

    created add_property query

---
+++ server/db/add_property.sql
@@ -0,0 +1,3 @@
+INSERT INTO property (property_name, property_description, address, city, state, zipcode, image_url, loan_amount, monthly_mortgage, user_id)
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);
+SELECT * FROM property;
